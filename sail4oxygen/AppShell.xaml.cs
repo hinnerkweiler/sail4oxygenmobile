ce18323525b39721d547d438b569b74006b9e586
Hinnerk Weiler <post@hinnerk-weiler.de>
2023-06-15 08:23:44 +0200

    Init

---
+++ sail4oxygen/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace sail4oxygen;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
---
+++ sail4oxygen/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace sail4oxygen;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+
