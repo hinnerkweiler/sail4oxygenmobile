e9668dd62c24ffac1c60471d81ee445d02c04272
Hinnerk Weiler <post@hinnerk-weiler.de>
2023-07-13 11:26:25 +0200

    Copy a shared File to local app space

--- sail4oxygen/Platforms/Android/MainActivity.cs
+++ sail4oxygen/Platforms/Android/MainActivity.cs
@@ -1,15 +1,53 @@
 ﻿using Android.App;
 using Android.Content.PM;
 using Android.OS;
+using Android.Content;
+
+
 
 namespace sail4oxygen;
 
 [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
-//Recive Share intent from other apps for csv files
-[IntentFilter(new[] { Intent.ActionSend }, Categories = new[] { Intent.CategoryDefault }, DataMimeType = @"*/*")]
+[IntentFilter(new[] { Intent.ActionSend }, Categories = new[] { Intent.CategoryDefault }, DataMimeType = @"*/*", Label = "Send to GEOMAR")]
 
 public class MainActivity : MauiAppCompatActivity
 {
+    public static string CsvFromShare { get; set; }
+
+    protected override void OnCreate(Bundle savedInstanceState)
+    {
+        base.OnCreate(savedInstanceState);
+
+        var intent = Intent;
+        var action = intent.Action;
+        var type = intent.Type;
+
+    
+        //store the intent data in a static variable
+        if (Intent.ActionSend.Equals(action) && type != null)
+        {
+            if ("text/comma-separated-values".Equals(type))
+            {   
+                var uri = (Android.Net.Uri)intent.GetParcelableExtra(Intent.ExtraStream);
+
+#if DEBUG
+                System.Console.WriteLine("CSV Recived ********************************************************" + uri);
+#endif
+                var appFilePath = AndroidHelpers.UriResolver.CopyFileFromUriToAppSpace(this, uri);
+
+                if (!string.IsNullOrEmpty(appFilePath.LocalPath))
+                {
+                    // Use the app file path as needed
+                    Console.WriteLine($"App file path: {appFilePath}");
+                }
+
+                Models.SharedData.FileUri = appFilePath;
+            }
+
+        }
+        
+    }
+
     protected override void OnResume()
     {
         base.OnResume();
---
+++ sail4oxygen/Platforms/Android/UriResolver.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Android.Content;
+using Java.IO;
+
+//Android Specific code
+//Copy a shared file to my local App storage space
+
+namespace sail4oxygen.AndroidHelpers
+{
+	public static class UriResolver
+	{
+        public static System.Uri CopyFileFromUriToAppSpace(Context context, Android.Net.Uri uri)
+        {
+            string appFilePath = null;
+
+            try
+            {
+                string scheme = uri.Scheme;
+
+                if (ContentResolver.SchemeContent.Equals(scheme))
+                {
+#if DEBUG
+                    System.Console.WriteLine("************ in first part resolver");
+#endif
+                    // Create an InputStream from the content URI
+                    using (var inputStream = context.ContentResolver.OpenInputStream(uri))
+                    {
+                        // Create a temporary file in your app's private directory
+                        var tempDir = context.GetExternalFilesDir(null);
+                        var tempFile = new Java.IO.File(tempDir, "temp_file.csv");
+
+                        // Copy the file from the InputStream to the temporary file
+                        using (var outputStream = new FileOutputStream(tempFile))
+                        {
+                            byte[] buffer = new byte[4096];
+                            int bytesRead;
+                            while ((bytesRead = inputStream.Read(buffer)) > 0)
+                            {
+                                outputStream.Write(buffer, 0, bytesRead);
+                            }
+                        }
+                        System.Console.WriteLine("************ finished copy");
+
+                        // Get the absolute path of the temporary file
+                        appFilePath = tempFile.AbsolutePath;
+                    }
+                }
+                else if (ContentResolver.SchemeFile.Equals(scheme))
+                {
+                    // Get the file path directly from the URI
+                    appFilePath = uri.Path;
+                }
+                else if (ContentResolver.SchemeContent.Equals(scheme))
+                {
+                    // Handle other schemes as needed
+                    // Additional logic may be required to handle specific URI schemes
+                }
+            }
+            catch (Exception ex)
+            {
+                // Handle any exceptions that may occur during file copying
+                System.Console.WriteLine($"Error copying file from URI to app space: {ex.Message}");
+            }
+
+            return new Uri(appFilePath);
+        }
+    }
+}
+
+
