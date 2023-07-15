e687921b3bca9ad49152c1bf619c1f617d0b0a2f
Hinnerk Weiler <post@hinnerk-weiler.de>
2023-07-15 22:28:45 +0200

    Verify the right File is used and has recent data

--- sail4oxygen/Models/CSVHelper.cs
+++ sail4oxygen/Models/CSVHelper.cs
@@ -12,24 +12,91 @@ namespace sail4oxygen.Models
             {
                 string csvText = await File.ReadAllTextAsync(file.LocalPath);
                 string[] csvLines = csvText.Split('\n');
-                string[] csvHeader = csvLines[0].Split(',');
-                string[] csvValues = csvLines[1].Split(',');
 
-                string newCsvText = csvLines[0] + ";Latitude;Longitude\n";
-
-                for (int i = 1; i < csvLines.Length; i++)
+                if (await VerifyCsvIsValidFile(csvLines))
                 {
-                    newCsvText += csvLines[i] + "," + location.Latitude.ToString("0.000000", System.Globalization.CultureInfo.InvariantCulture) + "," + location.Longitude.ToString("0.000000", System.Globalization.CultureInfo.InvariantCulture) + "\n";
+                    string newCsvText = csvLines[0];
+                    for (int i = 0; i < csvLines.Length; i++)
+                    {
+                        
+                        switch (i)
+                        {
+                            //Copy first n lines
+                            case < headerRowNumber:
+                                newCsvText += csvLines[i];
+                                break;
+                            //add Lat Lon in Header 
+                            case headerRowNumber:
+                                newCsvText += csvLines[i] += ";Latitude;Longitude\n";
+                                break;
+                            //add LatLonValue to each row past 
+                            default:
+                                newCsvText += csvLines[i] += ";" + location.Latitude.ToString("0.000000", System.Globalization.CultureInfo.InvariantCulture) + ";" + location.Longitude.ToString("0.000000", System.Globalization.CultureInfo.InvariantCulture) + "\n";
+                                break;
+                        }
+                    }
+
+                    await File.WriteAllTextAsync(file.LocalPath, newCsvText);
+
+                    return true;
                 }
 
-                await File.WriteAllTextAsync(file.LocalPath, newCsvText);
-
-                return true;
+                return false;
             }
             catch (Exception ex)
             {
-                Console.WriteLine("Something went wrong: ", ex.Message);
+                Console.WriteLine("CsvHelper.AddLocation – Something went wrong: ", ex.Message);
             }
+#if DEBUG
+            Console.WriteLine("CsvHelper.Addlocation – Something went wrong");
+#endif
+            return false;
+        }
+
+        static async Task<bool> VerifyCsvIsValidFile (string[] fileContent)
+        { 
+            try {
+                //correct file content
+                if (fileContent[1].StartsWith("Kor MEASUREMENT DATA FILE EXPORT") && 
+                    fileContent[11].StartsWith("Date (MM/DD/YYYY)") && 
+                    (fileContent[11].IndexOf(';') > -1))
+                {
+                    //recent measurement
+                    //find timestamp in row 4 column 2 and check if it is file is recent
+                    var measure = fileContent[headerRowNumber].Split(';');
+                    var timestamp = DateTime.Parse(measure[0]+ " " + measure[1]);
+                    var fileage = DateTime.Now - timestamp;
+#if DEBUG
+                    Console.WriteLine("VerifyCsvIsValidFile – Measurement timestamp: ", timestamp.ToString());
+#endif
+                    if (fileage.Minutes > 45)
+                    {
+                        if (await Application.Current.MainPage.DisplayAlert("File too old", $"Your measurement is {fileage} Minutes old. Are you still nearby where you took it or have you adjusted the coordinates before sending? ", "Yes","No"))
+                        {
+                            return true;
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    }
+                    else
+                    {
+                        return true;
+                    }
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("VerifyCsvIsValidFile – Something went wrong: ", ex.Message);
+            }   
+#if DEBUG
+            Console.WriteLine("VerifyCsvIsValidFile – File is not valid");
+#endif
             return false;
         }
     }
