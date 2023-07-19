dd619bc294c8aebb364a66dee67a345cb65c7dc4
Hinnerk Weiler <post@hinnerk-weiler.de>
2023-07-19 09:23:19 +0200

    Translate OnboardingPage

--- sail4oxygen/Views/Onboarding.xaml.cs
+++ sail4oxygen/Views/Onboarding.xaml.cs
@@ -9,11 +9,6 @@ public partial class Onboarding : ContentPage
 		InitializeComponent();
 	}
 
-    async void faq_Clicked(System.Object sender, System.EventArgs e)
-    {
-        await Shell.Current.GoToAsync("FaqPage");
-    }
-
     async void register_Clicked(System.Object sender, System.EventArgs e)
     {
         try
@@ -26,23 +21,23 @@ public partial class Onboarding : ContentPage
             Console.WriteLine(ex);
         }
 
-    }
-
-    
+    }    
 
 
     async void authorize_Clicked(System.Object sender, System.EventArgs e)
     {
-        // Save E-Mail and Registration to secure storage
-        // Call API to Confirm App in Use
-        await DisplayAlert("Registration", "Backend is not ready jet but for now let us pretend we did some server side magic and... Great, You are good to go.", "OK");
+        await DisplayAlert("Registration", "", "OK");
         await Shell.Current.GoToAsync("..");
     }
+
+
 
     async void exit_Clicked(System.Object sender, System.EventArgs e)
     {
         await Shell.Current.GoToAsync("..");
     }
+
+
 
     async void installkor_Clicked(System.Object sender, System.EventArgs e)
     {
