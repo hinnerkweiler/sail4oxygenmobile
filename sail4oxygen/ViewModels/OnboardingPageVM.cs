71f4ff4258ecab9f52d199183d99aab475f37264
Hinnerk Weiler <post@hinnerk-weiler.de>
2023-08-22 17:48:40 +0200

    Adding A Field to enter a Boat Name

--- sail4oxygen/ViewModels/OnboardingPageVM.cs
+++ sail4oxygen/ViewModels/OnboardingPageVM.cs
@@ -4,10 +4,47 @@ using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace sail4oxygen.ViewModels
 {
-	
-	
-	public partial class OnboardingPageVM : ObservableObject
-	{
+
+
+    public partial class OnboardingPageVM : ObservableObject
+    {
+        public bool BoatNameNotHidden
+        {
+            get => !BoatNameHidden;
+        }
+
+
+        public string BoatName
+        {
+            get
+            {
+                return Preferences.Get("BoatName", "Anonym");
+            }
+            set
+            {
+                //ToDo: Sanitizing??
+                Preferences.Set("BoatName", value);
+                OnPropertyChanged(nameof(BoatName));
+            }
+        }
+
+
+        public bool BoatNameHidden
+        {
+            get
+            {
+                return Preferences.Get("BoatNameHidden", false);
+            }
+            set
+            {
+                Preferences.Set("BoatNameHidden", value);
+                OnPropertyChanged(nameof(BoatNameHidden));
+                OnPropertyChanged(nameof(BoatName));
+            }
+        }
+
+
+
         public string ManualDate
         {
             get
@@ -17,28 +54,28 @@ namespace sail4oxygen.ViewModels
             }
         }
 
-		public string VersionInfo
-		{
-			get
-			{
-				return "Version " + VersionTracking.CurrentVersion;
-			}
-		}
+        public string VersionInfo
+        {
+            get
+            {
+                return "Version " + VersionTracking.CurrentVersion;
+            }
+        }
 
 
         public bool DsgvoBoxChecked
-		{
-			get => Models.DsgvoHandler.IsDsgvoAccepted;
-			set
-			{
-				Models.DsgvoHandler.IsDsgvoAccepted = value;
-			}
+        {
+            get => Models.DsgvoHandler.IsDsgvoAccepted;
+            set
+            {
+                Models.DsgvoHandler.IsDsgvoAccepted = value;
+            }
         }
 
-		public OnboardingPageVM()
-		{
+        public OnboardingPageVM()
+        {
 
-		}
-	}
+        }
+    }
 }
 
