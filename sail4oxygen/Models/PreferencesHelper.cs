c55e546074402bb5ce19396b259727a65744f910
Hinnerk Weiler <post@hinnerk-weiler.de>
2023-08-23 11:00:18 +0200

    static class to handle Preference Settings

---
+++ sail4oxygen/Models/PreferencesHelper.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using CommunityToolkit.Mvvm.ComponentModel;
+namespace sail4oxygen.Models
+{
+	public static partial class PreferencesHelper  
+	{
+        public static bool BoatNameNotHidden 
+        {
+            get => !BoatNameHidden;
+        }
+
+
+        public static string BoatName
+        {
+            get
+            {
+                if (BoatNameNotHidden)
+                {
+                    return Preferences.Get("BoatName", "Anonym");
+                }
+                return "Anonym";
+            }
+
+            set
+            {
+                Preferences.Set("BoatName", value);
+            }
+        }
+
+
+        public static bool BoatNameHidden
+        {
+            get
+            {
+                return Preferences.Get("BoatNameHidden", false);
+            }
+            set
+            {
+                Preferences.Set("BoatNameHidden", value);
+            }
+        }
+    }
+}
+
