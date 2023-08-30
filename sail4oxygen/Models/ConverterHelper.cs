d39dcb30b11a09faf9e66c4502c5eedbaa73ee71
Hinnerk Weiler <post@hinnerk-weiler.de>
2023-08-30 17:24:28 +0200

    Creating a Converter to handle manual input in XAML

---
+++ sail4oxygen/Models/ConverterHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+namespace sail4oxygen.Models
+{
+	public class ConverterHelper : IValueConverter
+	{
+            public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+            {
+            return value;
+            }
+
+            public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            {
+                if (value is string stringValue)
+                {
+                    if (double.TryParse(stringValue, NumberStyles.AllowDecimalPoint | NumberStyles.AllowThousands, new CultureInfo("en-US"), out double result))
+                    {
+                        return result;
+                    }
+                }
+
+                return Binding.DoNothing;
+            }
+        }
+}
+
