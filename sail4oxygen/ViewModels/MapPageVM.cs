c00d754a0946deb9cfc9e289535bbbb5fb66fdbc
Hinnerk Weiler <post@hinnerk-weiler.de>
2023-09-17 00:47:34 +0200

    mapview page

---
+++ sail4oxygen/ViewModels/MapPageVM.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace sail4oxygen.ViewModels
+{
+	public partial class MapPageVM : ObservableObject
+	{
+		[ObservableProperty]
+		ObservableCollection<Models.Port> portList = new();
+
+		public MapPageVM()
+		{
+			_= Init();
+		}
+
+		public async Task Init()
+		{
+			PortList = await Models.MapHelper.GetPortsFromFile();
+#if DEBUG
+			Console.WriteLine("Portlist updated:");
+			foreach (var port in PortList)
+			{
+				Console.WriteLine(port.latitude + " " + port.longitude);
+			}
+#endif
+		}
+	}
+}
+
---
+++ sail4oxygen/Views/MapPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace sail4oxygen.Views;
+
+public partial class MapPage : ContentPage
+{
+    public static ViewModels.MapPageVM MapPageViewModel = new();
+
+    public MapPage()
+	{
+		BindingContext = MapPageViewModel;
+		InitializeComponent();
+	}
+}
