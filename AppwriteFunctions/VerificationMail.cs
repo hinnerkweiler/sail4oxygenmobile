d1ddd2aa03b7d2376a88951455dae43551fa9c9a
Hinnerk Weiler <post@hinnerk-weiler.de>
2023-10-12 00:18:03 +0200

    init

--- AppwriteFunctions/VerificationMail.cs
+++ AppwriteFunctions/VerificationMail.cs
@@ -1,7 +1,26 @@
-﻿var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+﻿namespace DotNetRuntime;
 
-app.MapGet("/", () => "Hello World!");
+using Appwrite;
+using Appwrite.Services;
+using Appwrite.Models;
+using Appwrite.Extensions;
 
-app.Run();
+using Newtonsoft.Json.Linq;
+using System.Text.Json;
+using System.Collections;
 
+public class Handler
+{
+
+    // This is your Appwrite function
+    // It is executed each time we get a request
+    public async Task<RuntimeOutput> Main(RuntimeContext Context)
+    {
+        Context.Log(JsonSerializer.Serialize<object>(Context.Req.Headers));
+
+        //get user Id from Message
+        //get user
+        //create Verification
+        //Send Mail to User
+    }
+}
