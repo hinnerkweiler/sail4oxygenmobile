bb87107ea2cedd69a8ad4f670e455b4a56893ff5
Hinnerk Weiler <post@hinnerk-weiler.de>
2024-07-19 23:09:26 +0200

    adding a Location Service

--- sail4oxygen/App.xaml.cs
+++ sail4oxygen/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace sail4oxygen;
+﻿using sail4oxygen.Services;
+
+namespace sail4oxygen;
 
 public partial class App : Application
 {
@@ -7,14 +9,17 @@ public partial class App : Application
     public App()
 	{
 		InitializeComponent();
+	// Access the singleton instance to ensure it is initialized early
+		var locationService = LocationService.Instance;
 
 		MainPage = new AppShell();
+		
 	}
 
 	protected override void OnStart()
 	{
 		base.OnStart();
-			
+		var locationService = LocationService.Instance;
 	}
 
 	protected override void OnResume()
